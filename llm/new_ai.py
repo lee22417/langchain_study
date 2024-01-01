245bcd26875ff8657a7c4918da19d2a0968a81bc
jiwon <jlee22417@gmail.com>
2024-01-02 03:36:36 +0900

    ai - db 연결, 스키마 가져오는 함수, 쿼리 실행 함수 추가,,,ai가 질문에 자동으로 쿼리 만드는 함수 추가

--- llm/new_ai.py
+++ llm/new_ai.py
@@ -11,7 +11,6 @@ from langchain.prompts.chat import HumanMessagePromptTemplate
 import pymysql
 import pandas as pd
 import logging
-import re
 
 
 # from llm.llmcallback import ChatCallbackHandler
@@ -19,52 +18,102 @@ import re
 
 class new_ai:
     def __init__(self):
+        #  Openai information
         OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-        # HOST = st.secrets["host"]
-        # PORT = st.secrets["port"]
-        # USER = st.secrets["user"]
-        # PASSWORD = st.secrets["password"]
-        # DATABASE = st.secrets["database"]
-        # self.connection = pymysql.connect(
-        #     host=HOST,
-        #     port=int(PORT),
-        #     user=USER,
-        #     password=PASSWORD,
-        #     database=DATABASE
-        # )
         self.llm = ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo', verbose=True, streaming=True,
-            callbacks=[
-                # ChatCallbackHandler(),
-        ],)
-    
-    def translate_msg(self, ask):
+                            callbacks=[
+                                # ChatCallbackHandler(),
+                            ],)
+        # DB information
+        HOST = st.secrets["host"]
+        PORT = st.secrets["port"]
+        USER = st.secrets["user"]
+        PASSWORD = st.secrets["password"]
+        DATABASE = st.secrets["database"]
+        self.connection = pymysql.connect(
+            host=HOST,
+            port=int(PORT),
+            user=USER,
+            password=PASSWORD,
+            database=DATABASE
+        )
+
+    # DB 스키마 가져오는 함수
+    def get_schema(self, _):
+        sql = """
+            SELECT 
+                table_name, table_comment
+            FROM
+                information_schema.tables
+            WHERE
+                table_schema = 'hairdb'
+            """
+        schema = self.run_query(sql)
+        return schema
+
+    # DB 쿼리 실행 함수
+    def run_query(self, query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cursor.close()
+        return result
+
+    # Ai가 질문을 다른 언어로 변역하는 함수
+    def translate_msg(self, ask, language):
         try:
             result = []
-            
-            # ai가 실행할 행동 정의 (영어번역하기)
+
+            # ai가 실행할 행동 정의 (질문을 입력한 언어로 번역하기)
             translate = """
-            Translate the incoming Question into English as accurate as possible.
+            Translate the incoming Question into the language below as accurate as possible.
+            {language}
+            
             Question: {question}
             """
-            
-            # ai가 실행할 행동 정의 (3개의 다른 버전의 영어로 번역해서 가장 정확한거 선택)
-            # translate = """
-            # Translate the incoming question into English in three different versions as accurate as possible 
-            # and pick the only one translated English sentence that is the most accurate.
-            # Question: {question}
-            # """
-            
-            # 정의한 행동 요청 (영어번역 행동 요청)
+
+            # 정의한 행동 요청 (질문을 입력한 언어로 번역 요청)
             translate_prompt = ChatPromptTemplate.from_template(translate)
             translate_response = (
                 translate_prompt
                 | self.llm
             )
-            
-            # 위에 정의한 행동 실행 (영어번역 실행)
-            result = translate_response.invoke({"question": ask})
+
+            # 위에 정의한 행동 실행 (질문을 입력한 언어로 번역 실행)
+            result = translate_response.invoke(
+                {"question": ask, "language": language})
             print(f'convert to eng : ${result}')
             return result.content
         except Exception as e:
             logging.warning(e)
-            return ['Error to translate to English']
+            return 'Error: fail to translate to '+language
+
+    # Ai가 질문에 적절한 쿼리문 만드는 함수
+    def create_query(self, ask):
+        try:
+            result = []
+
+            # ai가 실행할 행동 정의 (쿼리문 작성)
+            translate = """
+            Based on the table schema below, write a SQL query that would answer the question:
+            {schema}
+
+            Question: {question}
+            SQL Query:
+            """
+
+            # 정의한 행동 요청 (쿼리문 작성 요청)
+            prompt = ChatPromptTemplate.from_template(translate)
+            response = (
+                RunnablePassthrough.assign(schema=self.get_schema)
+                | prompt
+                | self.llm
+            )
+
+            # 위에 정의한 행동 실행 (쿼리문 작성 실행)
+            result = response.invoke({"question": ask})
+            print(f'sql query : ${result}')
+            return result.content
+        except Exception as e:
+            logging.warning(e)
+            return 'Error: fail to create a query'
